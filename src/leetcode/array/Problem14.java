6aff82d985c6469a554ecfe71e5dd189d15b171d
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-01-06 08:09:07 -0500

    leetcode 14

--- src/leetcode/array/Problem14.java
+++ src/leetcode/array/Problem14.java
@@ -13,11 +13,14 @@ class Problem14Solution {
     public String longestCommonPrefix(String[] strs) {
 
         StringBuilder sb = new StringBuilder("");
+        // let's just assume first word is the smallest.
+        // because we return if there is an even smaller word
         String smallWord = strs[0];
 
         for (int i = 0; i < smallWord.length(); i++) {
 
             for (String word : strs) {
+                // if run out of bound for current word, then return
                 if (word.length() == i || word.charAt(i) != smallWord.charAt(i)) {
                     System.out.println("result=" + sb.toString());
                     return sb.toString();
