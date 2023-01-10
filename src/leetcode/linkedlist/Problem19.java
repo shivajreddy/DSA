d0135cb45b5593a6a0ddb6742ae3ffba5294de88
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-01-10 10:04:33 -0500

    leetcode 19

---
+++ src/leetcode/linkedlist/Problem19.java
@@ -0,0 +1,38 @@
+package leetcode.linkedlist;
+
+public class Problem19 {
+}
+
+class Problem19Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        int size = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            size++;
+            curr = curr.next;
+        }
+        int target = size - n;
+
+        // if the first item to be removed, then return 2nd item in list
+        // technically head still is attached to 2nd element. but its ok.
+        if (target == 0) return head.next;
+
+        ListNode dummyHead = new ListNode(0);
+        curr = head;
+        dummyHead.next = curr;
+        int i = 0;
+        while (curr != null) {
+            if (i == target - 1) {
+                if (curr.next.next != null) {
+                    curr.next = curr.next.next;
+                } else {
+                    curr.next = null;
+                }
+            }
+            i++;
+            curr = curr.next;
+        }
+        return dummyHead.next;
+    }
+}
