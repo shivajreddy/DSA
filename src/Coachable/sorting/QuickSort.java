31397b148f87f9c5cf559ce9ee59c51edcfa1ece
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-01-30 18:29:57 -0500

    quick sort using for loop and two pointers

--- src/Coachable/sorting/QuickSort.java
+++ src/Coachable/sorting/QuickSort.java
@@ -8,7 +8,9 @@ public class QuickSort {
 
 	public static void main(String[] args) {
 
-		int[] arr = new int[]{50, 70, 60, 90, 40, 80, 10, 20, 30, 9999};
+		//int[] arr = new int[]{50, 70, 60, 90, 40, 80, 10, 20, 30, 9999};
+		//int[] arr = new int[]{69, 65, 83, 89, 81, 85, 69, 83, 84, 73, 79, 78, 9999};
+		int[] arr = new int[]{69, 65, 83, 89, 81, 85, 69, 83, 84, 73, 79, 78};
 		System.out.println(Arrays.toString(arr));
 
 
@@ -25,14 +27,48 @@ public class QuickSort {
 			int partitionIdx = partition(arr, low, high);
 
 			// recursive call on the left half
-			QuickSorting(arr, low, partitionIdx);
+			//QuickSorting(arr, low, partitionIdx);
+
+			// recursive all on the left half
+			QuickSorting(arr, low, partitionIdx - 1);
 
 			// recursive call on the right half
 			QuickSorting(arr, partitionIdx + 1, high);
 		}
 	}
 
-	private static int partition(int[] arr, int l, int h) {
+
+	private static int partition(int[] arr, int low, int high) {
+		// assume last element is the pivot
+		int pivot = arr[high];
+
+		// pointer 'i' tracks the latest element that was smaller than pivot
+		int i = low - 1;
+		// pointer 'j' goes from low to the high-1, since pivot is at high
+		for (int j = low; j < high; j++) {
+
+			// search for item that is <= pivot
+			if (arr[j] <= pivot) {
+
+				// move 'i', since i current points to the element that is smaller than pivot
+				i++;
+
+				// swap the elements at i and j
+				int temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
+
+		// move 'i', swap that element with pivot, and return i
+		i++;
+		arr[high] = arr[i];
+		arr[i] = pivot;
+
+		return i;
+	}
+
+	private static int partitionUsingWhileLoop(int[] arr, int l, int h) {
 
 		int pivot = arr[l];
 
