0b51230dcfeae6c2e9da5f55c5d9d3097ed1ae36
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-02-02 10:07:54 -0500

    merge sort revision

--- src/Coachable/sorting/MergeSort.java
+++ src/Coachable/sorting/MergeSort.java
@@ -14,11 +14,15 @@ public class MergeSort {
 
 	private static void sort(int[] arr, int[] aux, int low, int high) {
 
-		if (high <= low) return;
+		// base condition for recursive calls
+		// low shouldn't touch high, i.e., if they do, then it's a single element
+		// minimum requirement is 2 elements for merging
+		if (low >= high) return;
 
-		// split the array into sub-arrays, sort them
+		// mid-point to split the array into two halves
 		int mid = low + ((high - low) / 2);
 
+		// split the array into sub-arrays, sort them
 		// recursively sort the left half and right half
 		sort(arr, aux, low, mid);
 		sort(arr, aux, mid + 1, high);
@@ -27,23 +31,28 @@ public class MergeSort {
 		merge(arr, aux, low, mid, high);
 	}
 
-	private static void merge(int[] originalArr, int[] auxiliaryArr, int low, int mid, int high) {
+	private static void merge(int[] originalArr, int[] auxArr, int low, int mid, int high) {
+
+		if (high - low + 1 < 1) return;
 
 		// copy to aux[]
-		if (high - low + 1 >= 0) System.arraycopy(originalArr, low, auxiliaryArr, low, high + 1 - low);
+		System.arraycopy(originalArr, low, auxArr, low, high + 1 - low);
 
+		// two pointers. i: goes from low to mid, j: goes from mid to high
 		int i = low, j = mid + 1;
+
+		// k: goes from low to high (high is index, k should go until high)
 		for (int k = low; k <= high; k++) {
 
 			// 'i' went out of bounds, meaning, still items left in 2nd half
-			if (i > mid) originalArr[k] = auxiliaryArr[j++];
+			if (i > mid) originalArr[k] = auxArr[j++];
 
 				// j went out of bounds, meaning, still items left in 1st half
-			else if (j > high) originalArr[k] = auxiliaryArr[i++];
+			else if (j > high) originalArr[k] = auxArr[i++];
 
 				//add the smallest of both arrays
-			else if (auxiliaryArr[i] < auxiliaryArr[j]) originalArr[k] = auxiliaryArr[i++];
-			else originalArr[k] = auxiliaryArr[j++];
+			else if (auxArr[i] < auxArr[j]) originalArr[k] = auxArr[i++];
+			else originalArr[k] = auxArr[j++];
 		}
 	}
 }
