33f60c2b1a0771344669bc55be8d347435941124
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-02-06 06:44:30 -0500

    stack, queue - started

---
+++ src/Coachable/DS/StackAndQueue/Reverse.java
@@ -0,0 +1,14 @@
+package Coachable.DS.StackAndQueue;
+
+import edu.princeton.cs.algs4.StdIn;
+
+
+public class Reverse {
+
+
+	public static void main(String[] args) {
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+		}
+	}
+}
