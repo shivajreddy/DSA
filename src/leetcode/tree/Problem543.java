a72eb37a7505ba93a8705b78469d09731e102724
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-02-08 11:30:18 -0500

    linkedlist - #143

---
+++ src/leetcode/tree/Problem543.java
@@ -0,0 +1,16 @@
+package leetcode.tree;
+
+
+public class Problem543 {
+	public static void main(String[] args) {
+
+	}
+}
+
+
+class Problem543Solution {
+	public int diameterOfBinaryTree(TreeNode root) {
+
+		return -1;
+	}
+}
