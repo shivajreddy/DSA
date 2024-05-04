b62b506eaeabb59555fc244a6656ca518968dee1
shivajreddy <shivajreddy@outlook.com>
2024-05-04 15:50:47 -0400

    86-Partition List

--- src/leetcode/linkedlist/86-PartitionList/solution.py
+++ src/leetcode/linkedlist/86-PartitionList/solution.py
@@ -5,6 +5,33 @@ class ListNode:
         self.val = val
         self.next = next
 
+'''
+Workflow Timestamps
+1. Understand the Problem:
+2. Design&Verify Solution:
+3. Code & Pass Test Cases:
+
+Test Cases:
+IN : x=3    1-1  
+OUT: 1-1
+
+IN : x=3    4-5
+OUT: 4-5
+
+IN: x=3  1-2-1-5-4
+OUT: 1-2-1-5-4
+
+
+Lesson Learnt:
+Test Cases: Didn't add any new test cases. TestCases for this question should be
+1. waht if all are smaller than x. 2. what if all are >= than x
+3. what already partitioned
+
+Approach: Not simple. all over the place.
+- not sure where to start 'prev' pointer
+
+'''
+
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
 
@@ -17,26 +44,32 @@ class Solution:
 
         while curr:
 
+            # found a node < x
             if curr.val < x:
 
-                nxt = curr.next
+                nxt = curr.next     # safe-keeping
 
+                # add curr node to small linked list
                 small.next = curr
                 curr.next = None
 
+                # move pointers
                 small = small.next
                 curr = nxt
             
+            # found a node >= x
             else:
-                nxt = curr.next
+                nxt = curr.next     # safe-keeping
 
+                # add curr node to big linked list
                 big.next = curr
                 curr.next = None
 
-                big = big.next
+                # move pointers
+                big = big.next 
                 curr = nxt
         
+        # point big list to the end of small linked list
         small.next = d2.next
 
         return d1.next
-
