335ff2a830095ce53007d9361aace4bcc5d61992
shivajreddy <shivajreddy@outlook.com>
2024-05-04 15:49:03 -0400

    82-RemoveDuplicates

--- src/leetcode/linkedlist/82-RemoveDuplicatesFromSortedListII/solution.py
+++ src/leetcode/linkedlist/82-RemoveDuplicatesFromSortedListII/solution.py
@@ -1,3 +1,20 @@
+'''
+Workflow Timestamps
+1. Understand the Problem:
+2. Design&Verify Solution:
+3. Code & Pass Test Cases:
+
+
+IN : 1 1 1 2 3
+OUT: 2 3
+
+IN : 1 1
+OUT: _
+
+IN : _
+OUT: _
+
+'''
 
 from typing import List, Optional
 
@@ -7,16 +24,34 @@ class ListNode:
         self.val = val
         self.next = next
 
+
+
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+
+
+        dummy = ListNode(-1, head)
+
+        curr = head
+        unique = dummy
+
+        while curr:
+            if curr.next and curr.next.val == curr.val:
+                while curr.next and curr.next.val == curr.val:
+                    curr = curr.next
+                unique.next = curr.next
+            else:
+                unique = curr
+
+            curr = curr.next
+
+
+        return dummy.next
 
 
 s = Solution()
-
 head = ListNode(1, ListNode(1, ListNode(1, ListNode(2, ListNode(3, None)))))
 s.deleteDuplicates(head)
-
 head = ListNode(1, ListNode(2, ListNode(3, ListNode(3, ListNode(3, ListNode(4, ListNode(4, ListNode(5, None))))))))
 s.deleteDuplicates(head)
-
