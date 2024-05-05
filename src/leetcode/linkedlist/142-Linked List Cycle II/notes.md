97a836280585c79ed28a3ddbd56cb0027ef23639
shivajreddy <shivajreddy@outlook.com>
2024-05-05 18:37:46 -0400

    .

---
+++ src/leetcode/linkedlist/142-Linked List Cycle II/notes.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=95ZfuoSAUPI
+
+This video explains the mathematical proof
---
+++ src/leetcode/linkedlist/237-DeleteNodeInALinkedList/solution.py
@@ -0,0 +1,19 @@
+'''
+TimeStamps:
+1. Understand the Problem: 00:00 - 00:00 (00 MIN)
+2. Design&Verify Solution: 00:00 - 00:00 (00 MIN)
+3. Code & Pass Test Cases: 00:00 - 00:00 (00 MIN)
+
+'''
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val, next = None) -> None:
+        self.val = val
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        pass
