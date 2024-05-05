ad41d64e687e769479acbbcdc4d184c9e625f2e4
shivajreddy <shivajreddy@outlook.com>
2024-05-05 19:48:16 -0400

    237-Delete Node in a Linked List

--- src/leetcode/linkedlist/237-DeleteNodeInALinkedList/notes.md
+++ src/leetcode/linkedlist/237-DeleteNodeInALinkedList/notes.md
@@ -2,3 +2,8 @@
 [LeetCode](https://leetcode.com/problems/delete-node-in-a-linked-list/description/)
 
 
+- got it wrong and looked at solution
+  - my answer kept adding 'None' as the last node, that is because
+  i changed value of the last (n th) node to be None,
+  but what i should do is set n-1th node's next value to be none.
+- i did understand the problem well, but my implementation had error.
--- src/leetcode/linkedlist/237-DeleteNodeInALinkedList/solution.py
+++ src/leetcode/linkedlist/237-DeleteNodeInALinkedList/solution.py
@@ -9,6 +9,41 @@ TimeStamps:
 2. Design&Verify Solution: 00:00 - 00:00 (00 MIN)
 3. Code & Pass Test Cases: 00:00 - 00:00 (00 MIN)
 
+- head is not given
+- all nodes are unique
+- node can be head
+- node is NOT last node in linked list
+
+4 - 5 - 1 - 9
+    c   n
+
+4   1   1   9 
+        c   n 
+
+4   1   9   9
+            c   n
+
+4   1   9   None 
+                  c
+
+
+1 2 3
+-
+c n
+2 2 3
+  c n
+2 3 2
+    c n
+2 3 None 
+         c
+
+
+1 2
+  -  
+  c n
+1 x 
+    c
+
 '''
 
 from typing import Optional
@@ -18,7 +53,42 @@ class ListNode:
         self.val = val
         self.next = None
 
-
 class Solution:
     def deleteNode(self, node):
-        pass
+
+        # ''' Using single pointer
+        # start curr pointer at the node that is to be removed
+        curr = node
+
+        while curr.next:
+            nxt = curr.next     # mark the nxt node of curr pointer
+
+            # remove curr-node value, by replacing with next pointer's value(if any)
+            curr.val = nxt.val
+
+            # if curr is pointing to the n-1th node, then curr's next should be removed
+            if not nxt.next:
+                curr.next = None
+
+            # prev = curr
+            curr = nxt      # move the curr pointer
+        # '''
+
+
+        ''' Using two pointers
+        curr = node
+        prev = None
+
+        while curr.next:
+            nxt = curr.next
+
+            curr.val = nxt.val
+
+            prev = curr
+            curr = nxt
+        
+        # curr is now pointing to the last node, which is to be removed
+        # so prev is pointing to the n-1th node
+        prev.next = None
+
+        # '''
