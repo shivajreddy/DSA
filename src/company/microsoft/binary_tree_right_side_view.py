ff672784eb95053b956f3ce53fbc60999a8a930c
shiva <shivajreddy@outlook.com>
2024-11-04 11:19:28 -0500

    .

--- src/company/microsoft/binary_tree_right_side_view.py
+++ src/company/microsoft/binary_tree_right_side_view.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional, List
 
 
@@ -7,6 +8,51 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
+
+    # DFS
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        
+        result = []
+
+        def dfs(node, lvl):
+            if not node:
+                return
+
+            if len(result) - 1 < lvl:
+                result.append(0)
+
+            result[lvl] = node.val
+
+            dfs(node.left, lvl + 1)
+            dfs(node.right, lvl + 1)
+
+        dfs(root, 0)
+
+        return result
+
+    # BFS
+    def rightSideView_bfs(self, root: Optional[TreeNode]) -> List[int]:
+
+        if not root:
+            return []
+
+        q = deque([(root, 1)])
+        res = []
+        curr_lvl = 0
+
+        while q:
+            node, lvl = q.popleft()
+
+            # Only add the first node encountered at each level(right most)
+            if lvl > curr_lvl:
+                res.append(node.val)
+                curr_lvl = lvl
+
+            if node.right:
+                q.append((node.right, lvl + 1))
+            if node.left:
+                q.append((node.left, lvl + 1))
+
+        return res
