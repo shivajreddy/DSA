8c2bb5ed00e25662fb37958b7eaea42531ab8b3d
shiva <shivajreddy@outlook.com>
2024-11-04 20:44:18 -0500

    .

--- src/company/microsoft/daily_temperatures.py
+++ src/company/microsoft/daily_temperatures.py
@@ -2,20 +2,23 @@ from typing import List
 
 
 class Solution:
+    """
+    Time : O(n)
+    Space: O(n)
+    """
+
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
         answer = [0] * n
-
-        stack = [n - 1]
-
-        # Increasing mono stack
 
         # Next greater element
         # Decreasing mono stack
         stack = []
         for idx, num in enumerate(temperatures):
             while stack and temperatures[stack[-1]] < num:
-                answer[stack[-1]] = idx - stack[-1]
+                top_idx = stack.pop()
+                answer[top_idx] = idx - top_idx
+            stack.append(idx)
 
         return answer
 
---
+++ src/company/microsoft/longest_increasing_subsequence.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    """
+    Time : a
+    Space: a
+    """
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        pass
