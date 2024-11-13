b0b24a2e5f3d837ee4a8e425c1f19f1379df69f1
shiva <shivajreddy@outlook.com>
2024-11-12 23:09:20 -0500

    .

--- src/company/microsoft/longest_increasing_subsequence.py
+++ src/company/microsoft/longest_increasing_subsequence.py
@@ -8,4 +8,20 @@ class Solution:
     """
 
     def lengthOfLIS(self, nums: List[int]) -> int:
-        pass
+        res = 0
+        stack = []
+        for num in nums:
+            while stack and stack[-1] >= num:
+                stack.pop()
+            stack.append(num)
+            res = max(res, len(stack))
+
+        print(res)
+        return res
+
+
+# TESTS
+sol = Solution()
+assert sol.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
+assert sol.lengthOfLIS([0, 1, 0, 3, 2, 3]) == 4
+assert sol.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]) == 1
