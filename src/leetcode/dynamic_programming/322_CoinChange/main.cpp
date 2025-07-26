8975a954d75c7a038a86083bf5192d1c1e1635d7
smpl <sreddy@hey.com>
2025-07-26 13:20:38 -0400

    .

---
+++ src/leetcode/dynamic_programming/322_CoinChange/main.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/coin-change
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        return -1;
+    }
+}
+
+int main(){
+    Solution* s = new Solution();
+    vector<int> coins;
+    int amount;
+
+    coins = { 1, 2, 5 };
+    amount = 11;
+    cout << s->coinChange(coins, amount) << endl;
+
+    coins = { 2 };
+    amount = 3;
+    cout << s->coinChange(coins, amount) << endl;
+
+    coins = { 1 };
+    amount = 0;
+    cout << s->coinChange(coins, amount) << endl;
+
+    delete s;
+}
