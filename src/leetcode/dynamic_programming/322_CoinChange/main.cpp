29f7230a7410fcf3ebcddaa9f1d1d8eac73910fe
shiva <sreddy@hey.com>
2025-07-26 22:18:22 -0400

    .

--- src/leetcode/dynamic_programming/322_CoinChange/main.cpp
+++ src/leetcode/dynamic_programming/322_CoinChange/main.cpp
@@ -1,12 +1,30 @@
 // https://leetcode.com/problems/coin-change
 
 #include <bits/stdc++.h>
+#include <climits>
 using namespace std;
 
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        return -1;
+
+        unordered_map<int, int> hm;
+        function<int(long long)> rec = [&](long long curr_amount) -> int {
+            if (curr_amount > amount) return INT_MAX;
+            if (curr_amount == amount) return 0;
+            if (hm.find(curr_amount) != hm.end()) return hm[curr_amount];
+
+            long long val = INT_MAX;
+            for (int c : coins) {
+                long long res = rec(curr_amount + c);
+                if (res != INT_MAX) val = min(val, 1 + res);
+            }
+
+            return hm[curr_amount] = val;
+        };
+
+        int res = rec(0);
+        return res == INT_MAX ? -1 : res;
     }
 };
 
