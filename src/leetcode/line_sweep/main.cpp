99f518546cae33d7408e247b1fbdb489714ef93e
shiva <sreddy@hey.com>
2025-07-28 13:10:58 -0400

    .

--- src/leetcode/line_sweep/main.cpp
+++ src/leetcode/line_sweep/main.cpp
@@ -7,7 +7,29 @@ using namespace std;
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        return false;
+
+        function<void(vector<vector<int>>&)> print_intervals =
+            [&](vector<vector<int>>& v) -> void {
+            for (auto& interval : v) {
+                cout << "[" << interval[0] << " " << interval[1] << "] ";
+            }
+            cout << endl;
+        };
+        // print_intervals(intervals);
+
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[0] == b[0] ? a[1] < b[1] : a[0] < b[0];
+             });
+        // print_intervals(intervals);
+
+        int n = intervals.size();
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
@@ -22,6 +44,10 @@ int main() {
     }
     {
         intervals = { { 7, 10 }, { 2, 4 } };
+        cout << sol->canAttendMeetings(intervals) << endl;
+    }
+    {
+        intervals = { { 0, 30 }, { 0, 20 }, { 15, 20 } };
         cout << sol->canAttendMeetings(intervals) << endl;
     }
 
