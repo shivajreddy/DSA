0d34debbfb28317ff30e8c2d3603eb8341fa3af9
shiva <sreddy@hey.com>
2025-07-28 15:56:48 -0400

    .

--- src/leetcode/line_sweep/56_MergeIntervals/main.cpp
+++ src/leetcode/line_sweep/56_MergeIntervals/main.cpp
@@ -8,7 +8,26 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        return {};
+
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+
+        int left_most, right_most;
+        left_most = intervals[0][0], right_most = intervals[0][1];
+
+        vector<vector<int>> res;
+
+        for (int i = 1; i < n; i++) {
+            if (right_most < intervals[i][0]) {
+                res.push_back({ left_most, right_most });
+                left_most = intervals[i][0], right_most = intervals[i][1];
+            } else {
+                right_most = max(right_most, intervals[i][1]);
+            }
+        }
+        res.push_back({ left_most, right_most });
+
+        return res;
     }
 };
 
@@ -29,6 +48,10 @@ int main() {
     }
     {
         intervals = { { 1, 4 }, { 4, 5 } };
+        print_vvi(sol->merge(intervals));
+    }
+    {
+        intervals = { { 1, 4 }, { 5, 6 } };
         print_vvi(sol->merge(intervals));
     }
 
