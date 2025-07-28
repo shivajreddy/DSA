52eab41ad45e170a4d0cb577bfbfeb272215e12a
shiva <sreddy@hey.com>
2025-07-28 16:00:39 -0400

    .

---
+++ src/leetcode/line_sweep/57_InsertInterval/main.cpp
@@ -0,0 +1,59 @@
+// 57. Insert Interval
+// https://leetcode.com/problems/insert-interval
+
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
+
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+
+        int left_most, right_most;
+        left_most = intervals[0][0], right_most = intervals[0][1];
+
+        vector<vector<int>> res;
+
+        for (int i = 1; i < n; i++) {
+            if (right_most < intervals[i][0]) {
+                res.push_back({ left_most, right_most });
+                left_most = intervals[i][0], right_most = intervals[i][1];
+            } else {
+                right_most = max(right_most, intervals[i][1]);
+            }
+        }
+        res.push_back({ left_most, right_most });
+
+        return res;
+    }
+};
+
+int main() {
+    Solution* sol = new Solution();
+    vector<vector<int>> intervals;
+    vector<int> newInterval;
+
+    auto print_vvi = [&](vector<vector<int>> v) {
+        int n = v.size();
+        for (int i = 0; i < n; i++)
+            cout << "[" << v[i][0] << " " << v[i][1] << "] ";
+        cout << endl;
+    };
+
+    {
+        intervals = { { 1, 3 }, { 6, 9 } };
+        newInterval = { 2, 5 };
+        print_vvi(sol->insert(intervals, newInterval));
+    }
+    {
+        intervals = { { 1, 2 }, { 3, 5 }, { 6, 7 }, { 8, 10 }, { 12, 16 } };
+        newInterval = { 4, 8 };
+        print_vvi(sol->insert(intervals, newInterval));
+    }
+
+    delete sol;
+}
