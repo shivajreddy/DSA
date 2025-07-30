f7010dd84993ccbbce3a360e35f54e0515b679f9
smpl <sreddy@hey.com>
2025-07-29 20:45:24 -0400

    .

--- src/leetcode/line_sweep/452_MinArrowsBalloons/main.cpp
+++ src/leetcode/line_sweep/452_MinArrowsBalloons/main.cpp
@@ -7,6 +7,37 @@ using namespace std;
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
+
+        sort(points.begin(), points.end(),
+             [&](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        int n = points.size();
+        // NOTE: taking INT_MIN would fail when interval starts at INT_MIN, so
+        // use long long to track the previous shot end
+        long long prev_shot_end = -1e17;
+        int arrows = 0;
+        for (int i = 0; i < n; i++) {
+            int curr_start = points[i][0], curr_end = points[i][1];
+            if (prev_shot_end < curr_start) { // no overlap
+                //  so we need a new arrow for this
+                arrows++;
+                prev_shot_end = curr_end;
+            } else {
+                // currstart overlaps with previous shot
+                // use the same arrow. no need to increase arrows or
+                // update previous shot end
+            }
+        }
+
+        return arrows;
+    }
+};
+
+class SolutionMain {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end(),
              [&](const vector<int>& a, const vector<int>& b) {
                  return a[1] < b[1];
@@ -86,9 +117,12 @@ int main() {
         points = { { 2, 3 }, { 2, 3 } };
         cout << sol->findMinArrowShots(points) << endl;
     }
-    // */
     {
         points = { { -1, 1 }, { 0, 1 }, { 2, 3 }, { 1, 2 } };
+        cout << sol->findMinArrowShots(points) << endl;
+    }
+    {
+        points = { { -2147483648, 2147483647 } };
         cout << sol->findMinArrowShots(points) << endl;
     }
 
