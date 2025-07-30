6f34414183cc912bdb93004705e5da834b28b2f2
shiva <sreddy@hey.com>
2025-07-30 14:49:44 -0400

    .

--- src/leetcode/line_sweep/1229_MeetingScheduler/main.cpp
+++ src/leetcode/line_sweep/1229_MeetingScheduler/main.cpp
@@ -9,7 +9,45 @@ public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1,
                                      vector<vector<int>>& slots2,
                                      int duration) {
-        return { -1, -1 };
+        vector<pair<int, char>> events;
+
+        // Add all start and end events
+        for (const auto& slot : slots1) {
+            events.push_back({ slot[0], 's' });
+            events.push_back({ slot[1], 'e' });
+        }
+        for (const auto& slot : slots2) {
+            events.push_back({ slot[0], 's' });
+            events.push_back({ slot[1], 'e' });
+        }
+
+        sort(events.begin(), events.end());
+
+        int curr_overlap = 0;
+        int overlap_start = -1;
+
+        for (int i = 0; i < events.size(); i++) {
+            if (events[i].second == 's') { // start event
+                curr_overlap++;
+                if (curr_overlap == 2) {
+                    // Overlap period starts now
+                    overlap_start = events[i].first;
+                }
+            } else { // end event
+                if (curr_overlap == 2) {
+                    // We're ending an overlap period
+                    int overlap_end = events[i].first;
+                    int available_duration = overlap_end - overlap_start;
+
+                    if (available_duration >= duration) {
+                        return { overlap_start, overlap_start + duration };
+                    }
+                }
+                curr_overlap--;
+            }
+        }
+
+        return {}; // No suitable slot found
     }
 };
 
@@ -26,15 +64,19 @@ int main() {
     };
     auto printv = [&](vector<int> v) {
         for (int num : v) cout << num << " ";
+        cout << endl;
     };
 
     {
+        cout << "TESTCASE 1\n";
         slots1 = { { 10, 50 }, { 60, 120 }, { 140, 210 } };
         slots2 = { { 0, 15 }, { 60, 70 } };
         duration = 8;
         printv(sol->minAvailableDuration(slots1, slots2, duration));
     }
+
     {
+        cout << "TESTCASE 2\n";
         slots1 = { { 10, 50 }, { 60, 120 }, { 140, 210 } };
         slots2 = { { 0, 15 }, { 60, 70 } };
         duration = 12;
