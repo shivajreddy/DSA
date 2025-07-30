eb67d37b7cfe348f505acca19bfdca78be645758
smpl <sreddy@hey.com>
2025-07-29 21:44:10 -0400

    .

--- src/leetcode/line_sweep/646_MaxPairChain/main.cpp
+++ src/leetcode/line_sweep/646_MaxPairChain/main.cpp
@@ -7,7 +7,26 @@ using namespace std;
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        return -1;
+
+        sort(pairs.begin(), pairs.end(),
+             [&](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        int n = pairs.size();
+        int links = 1;
+        int prev_end = pairs[0][1];
+        for (int i = 1; i < n; i++) {
+            int curr_start = pairs[i][0], curr_end = pairs[i][1];
+            // case1: curr_start over laps with previous links end
+            if (prev_end >= curr_start) {
+                // we dont use the current link
+            } else { // case2: there is no overlap, consider this link
+                links++;
+                prev_end = curr_end;
+            }
+        }
+        return links;
     }
 };
 
