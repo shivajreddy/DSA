124c717efc5a852b632bc9ab3676f8d0f7a5ff5d
smpl <sreddy@hey.com>
2025-08-03 14:20:48 -0400

    .

--- src/leetcode_contests/461/q2/main.cpp
+++ src/leetcode_contests/461/q2/main.cpp
@@ -1,7 +1,14 @@
-// problem link
+// Q2. Maximum Balanced Shipments
+// https://leetcode.com/contest/weekly-contest-461/problems/maximum-balanced-shipments/
 
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    int maxBalancedShipments(vector<int>& weight) {
+    }
+};
 
 int main() {
 }
