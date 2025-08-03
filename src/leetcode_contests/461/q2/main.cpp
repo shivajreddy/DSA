7c5e84ea7dd9a7fdd13483fd3e9dcb90ef512836
smpl <sreddy@hey.com>
2025-08-03 14:48:09 -0400

    .

--- src/leetcode_contests/461/q2/main.cpp
+++ src/leetcode_contests/461/q2/main.cpp
@@ -7,8 +7,38 @@ using namespace std;
 class Solution {
 public:
     int maxBalancedShipments(vector<int>& weight) {
+        int n = weight.size();
+        int segs = 0;
+        int i = 1;
+        while (i < n) {
+            if (weight[i - 1] > weight[i]) {
+                segs++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return segs;
     }
 };
 
 int main() {
+    Solution* sol = new Solution();
+    vector<int> weight;
+    {
+        weight = { 2, 5, 1, 4, 3 };
+        cout << "EXPECTED: " << 2 << endl;
+        cout << "RECEIVED: " << sol->maxBalancedShipments(weight) << endl;
+    }
+    {
+        weight = { 4, 4 };
+        cout << "EXPECTED: " << 0 << endl;
+        cout << "RECEIVED: " << sol->maxBalancedShipments(weight) << endl;
+    }
+    {
+        weight = { 1000, 999, 998 };
+        cout << "EXPECTED: " << 1 << endl;
+        cout << "RECEIVED: " << sol->maxBalancedShipments(weight) << endl;
+    }
+    delete sol;
 }
