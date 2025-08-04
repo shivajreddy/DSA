7e5161566333e292e215f5b3968d6fffab05463f
shiva <sreddy@hey.com>
2025-08-04 11:42:01 -0400

    .

--- src/Max_Dist_To_GasStation/main.cpp
+++ src/Max_Dist_To_GasStation/main.cpp
@@ -8,6 +8,33 @@ class Solution {
 public:
     double findSmallestMaxDist(vector<int>& stations, int k) {
         // Code here
+        int n = stations.size();
+
+        // 3  6  12  19  33  44  67  72  89  95
+        for (int i = 1; i < n; i++) {
+            int d = stations[i] - stations[i - 1];
+            stations[i - 1] = d;
+        }
+        // 3  6   7  14  11  23  5   17  6  [95] <- we just ignore the last num
+
+        while (k) {
+            // find the best spot to place, which is the largest dist
+            int largest = -1, largest_idx = -1;
+            for (int i = 0; i < n - 1; i++) {
+                if (largest < stations[i]) {
+                    largest = stations[i];
+                    largest_idx = i;
+                }
+            }
+            stations[largest_idx] = stations[largest_idx] / 2;
+            k--;
+        }
+
+        int total_dist = 0;
+        for (int i = 0; i < n - 1; i++) {
+            total_dist += stations[i];
+        }
+
         return 0.0;
     }
 };
