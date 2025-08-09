d14153aee61f7d206cce6639cd66e93b521463f1
smpl <sreddy@hey.com>
2025-08-09 10:43:57 -0400

    .

--- src/other_lists/cses/intro_problems/weird_algorith.cpp
+++ src/other_lists/cses/intro_problems/weird_algorith.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// shorter type names
+typedef long long ll;
+typedef vector<int> vi;
+
+// Macros
+#define PB push_back
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
 int main() {
-    cout << "WEIRD ALGORITHM" << endl;
+    ll n;
+    cin >> n;
+
+    while (n != 1) {
+        cout << n << " ";
+        if (n % 2 == 0)
+            n /= 2;
+        else
+            n = n * 3 + 1;
+    }
+    cout << 1 << endl;
 }
